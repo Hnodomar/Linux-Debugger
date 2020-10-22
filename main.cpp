86c7177d5e57f23cbc466e44588241217cd745ca
George <bluesilverg22@gmail.com>
2020-10-22 18:48:23 +0100

    Parent/Child fork build, beginning of debugger class started

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class debugger { //To interact with child process
+    public:
+        debugger (std::string prog_name, pid_t pid)
+            : m_prog_name{std::move(prog_name)}, m_pid{pid} {}
+        
+        void run();
+    
+    private:
+        std::string m_prog_name;
+        pid_t m_pid;
+};
+
+void debugger::run() {
+    int wait_status;
+    auto options = 0;
+    waitpid(m_pid, &wait_status, options);
+
+    char* line = nullptr;
+    while((line = linenoise("minidbg> ")) != nullptr) {
+        handle_command(line);
+        linenoiseHistoryAdd(line);
+        linenoiseFree(line);
+    }
+}
+
+
+// fork/exec pattern
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Program to debug not specified";
+        return -1;
+    }
+    auto prog = argv[1];
+    auto pid = fork(); //Create new process (called a child process)
+    if (pid == 0) { //fork returns 0 to newly created child process
+    //child goes here - execute debugee
+        ptrace(PTRACE_TRACEME, 0, nullptr, nullptr); //allow parent to trace child
+        execl(prog, prog, nullptr); //analogous: ./prog prog i.e: ./fullPath processName
+    }
+    else if (pid >= 1) { //positive value returned by fork will be child process ID
+    //parent goes here - execute debugger
+        debugger dbg{prog, pid};
+        dbg.run();
+    }
+}
