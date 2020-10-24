407c25bdae4141eee4c4097b4158059f9894cbe7
Hnodomar <bluesilverg22@gmail.com>
2020-10-24 10:42:31 +0100

    Code refactor

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include "linenoise.h"
 
 #include <vector>
-#include <string>
-#include <utility>
+
 #include <iostream>
-#include <linux/types.h>
 #include <sys/ptrace.h>
-#include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <fstream>
-#include <iomanip>
 #include <sstream>
 #include <stddef.h>
-#include <unordered_map>
+
+#include "debugger.hpp"
+
+using namespace minidbg;
 
 //HELPER FUNCTIONS
 std::vector<std::string> split(const std::string &s, char delimiter) {
@@ -32,58 +31,6 @@ bool is_prefix(const std::string& s, const std::string& of) {
 	return std::equal(s.begin(), s.end(), of.begin());
 }
 //END HELPER FUNCTIONS
-class breakpoint {
-public:
-	breakpoint() = default;
-    breakpoint(pid_t pid, std::intptr_t addr)
-        : m_pid{pid}, m_addr{addr}, m_enabled{false}, m_saved_data{}
-    {}
-
-    void enable();
-    void disable();
-
-    auto is_enabled() const -> bool { return m_enabled; }
-    auto get_address() const -> std::intptr_t { return m_addr; }
-
-private:
-    pid_t m_pid;
-    std::intptr_t m_addr;
-    bool m_enabled;
-    uint8_t m_saved_data; //data which used to be at the breakpoint address
-};
-
-void breakpoint::enable() { //breakpoint member function to set breakpoint trap at desired address
-	auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr); //PTRACE_PEEKDATA: ptrace request to read memory of traced process
-	m_saved_data = static_cast<uint8_t>(data & 0xff); //save the bottom byte of original data at breakpt address
-	uint64_t int3 = 0xcc; //trap: breakpoint instruction
-	uint64_t data_with_int3 = ((data & ~0xff) | int3); //bottom byte send to 0xcc - breakpoint instruction
-	ptrace(PTRACE_POKEDATA, m_pid, m_addr, data_with_int3); //PTRACE_POKEDATA: ptrace request to write to memory
-	m_enabled = true;
-}
-
-void breakpoint::disable() { //ptrace memory requests operate on whole words instead of bytes
-	auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr); //so we need to read the word
-	auto restored_data = ((data & ~0xff) | m_saved_data);		 //at the location and then
-	ptrace(PTRACE_POKEDATA, m_pid, m_addr, restored_data);	     //overwrite the low byte with original data
-	m_enabled = false;
-}
-
-class debugger { //To interact with child process
-    public:
-        debugger (std::string prog_name, pid_t pid) //constructor to initialise m_prog_name & m_pid
-            : m_prog_name{std::move(prog_name)}, m_pid{pid} 
-		{}
-        
-        void run();
-		void set_breakpoint_at_address(std::intptr_t addr);
-		
-	private:
-		void continue_execution();
-		void handle_command(const std::string&);
-		std::unordered_map<std::intptr_t,breakpoint> m_breakpoints;
-        std::string m_prog_name;
-        pid_t m_pid;
-};
 
 void debugger::set_breakpoint_at_address(std::intptr_t addr) {
 	std::cout << "Set breakpoint at address 0x" << std::hex << addr << std::endl;
